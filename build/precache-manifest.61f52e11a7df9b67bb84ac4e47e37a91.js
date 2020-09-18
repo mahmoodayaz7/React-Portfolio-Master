c300a777da5620a995aa7990c255b441b4b55dcd
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2020-09-18 14:48:51 +0500

    Build
    Build Project

---
+++ build/precache-manifest.61f52e11a7df9b67bb84ac4e47e37a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49ab477bab217df67ea9aa8e6dfd250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bcb873bf2819f745067",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcb873bf2819f745067",
+    "url": "/static/js/main.fd30b571.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
